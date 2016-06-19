f53fcb4802c8ded58d47f6b1bdbd882a0795bbb9
Szymon Kłos <eszkadev@gmail.com>
2016-06-19 17:54:14 +0200

    conding style

--- source/twi_at16.c
+++ source/twi_at16.c
@@ -36,26 +36,26 @@
 void twi_init(void)
 {
 	DDRC &= ~((1 << PC4) | (1 << PC5));
-	TWBR = ((_CLOCK/SCL_CLOCK)-16)/2;
+	TWBR = ((_CLOCK/SCL_CLOCK) - 16)/2;
 }
 
 void twi_start(void)
 {
-	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
+	TWCR = (1 << TWINT) | ( 1<< TWSTA) | (1 << TWEN);
+	while(!(TWCR & (1 << TWINT)));
 }
 
 void twi_stop(void)
 {
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
-	while ((TWCR & (1<<TWSTO)));
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+	while((TWCR & (1 << TWSTO)));
 }
 
 void twi_write(uint8_t data)
 {
 	TWDR = data;
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while(!(TWCR & (1 << TWINT)));
 }
 
 uint8_t twi_read(ack_bit ack)
@@ -64,7 +64,7 @@ uint8_t twi_read(ack_bit ack)
 	TWCR = ack
 	? ((1 << TWINT) | (1 << TWEN) | (1 << TWEA))
 	: ((1 << TWINT) | (1 << TWEN)) ;
-	while (!(TWCR & (1<<TWINT)));
+	while(!(TWCR & (1 << TWINT)));
 	return TWDR;
 }
 
